e12d3e9a23bee812b495a80366fe7454b06b4034
Nguyenphangiang <phancruel@gmail.com>
2022-05-05 09:16:27 +0700

    up

---
+++ src/main/java/com/codegym/md4thspringboot/Md4ThSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.codegym.md4thspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Md4ThSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Md4ThSpringBootApplication.class, args);
+    }
+
+}
